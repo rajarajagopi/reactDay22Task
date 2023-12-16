02278d833ab4e4a5c2d50758442dd1313badd748
rajarajagopi <rajagopimca@gmail.com>
2023-12-16 21:15:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+function App() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+    
+      
+  )
+  
+}
+
+export default App
---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+
+function Dashboard() {
+  return (
+    
+    <div>
+        Hello DashBoard
+    </div>
+
+  )
+}
+
+export default Dashboard
